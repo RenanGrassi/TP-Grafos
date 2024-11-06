a81a0794e09e33cd5404e8021a944859613ee4d7
eduadz <eduardo.s.filho@ufv.br>
2024-11-06 14:01:17 -0300

    refactor: retirando teste de funções

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ def main():
     print("Ordem do Grafo:", grafo.ordem())
     print("Tamanho do Grafo:", grafo.tamanho())
     print("Densidade do Grafo:", grafo.densidade())
-    print("Vizinhos do vértice 2:", grafo.vizinhos(2))
-    print("Grau do vértice 5:", grafo.grau(5))
-    print("O vértice 2 é uma articulação: ", grafo.e_articulacao(2))
-    print("O vértice 5 é uma articulação: ", grafo.e_articulacao(5))
 
 if __name__ == "__main__":
     main()
