5c26ab51003ed6a89a486e4a8d7cc0a6b1f4986f
eduadz <eduardo.s.filho@ufv.br>
2024-11-06 13:59:38 -0300

    feat: adicionando prints para verificar funções

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ def main():
     #Arquivo grafo.txt já tem que estar criado!
     grafo = Grafo("grafo.txt") 
     
-    print("Ordem do Grafo:", grafo.ordem_do_grafo())
-    print("Tamanho do Grafo:", grafo.tamanho_do_grafo())
+    print("Ordem do Grafo:", grafo.ordem())
+    print("Tamanho do Grafo:", grafo.tamanho())
+    print("Densidade do Grafo:", grafo.densidade())
+    print("Vizinhos do vértice 2:", grafo.vizinhos(2))
+    print("Grau do vértice 5:", grafo.grau(5))
+    print("O vértice 2 é uma articulação: ", grafo.e_articulacao(2))
+    print("O vértice 5 é uma articulação: ", grafo.e_articulacao(5))
 
 if __name__ == "__main__":
     main()
