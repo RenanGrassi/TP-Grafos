23dc6d99e7575d050e547dd7bb4c480c146a5331
Eufrausino <gustavo.medeiros@ufv.br>
2024-11-13 21:00:28 -0300

    Função para determinar se há ou não ciclo

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from grafo import Grafo
 def main():
     #Arquivo grafo.txt já tem que estar criado!
     ##grafo = Grafo("grafo2.txt")
-    grafo = Grafo("grafo.txt")
+    grafo = Grafo("grafo2.txt")
     
     print("Ordem do Grafo:", grafo.ordem())
     print("Tamanho do Grafo:", grafo.tamanho())
     print("Densidade do Grafo:", grafo.densidade())
-    grafo.buscaEmLargura(12)
+    grafo.buscaEmLargura(2)
+    print(grafo.verifica_ciclo())
 
 if __name__ == "__main__":
     main()
